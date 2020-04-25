3ecd2baa0a05c26d9eef58b54f5514ea972e92b2
harrison314 <harrison314@azet.sk>
2020-04-25 12:29:49 +0200

    Refactor CertificateGenerator.

--- src/test/PkcsExtenions.Tests/CertificateGenerator.cs
+++ src/test/PkcsExtenions.Tests/CertificateGenerator.cs
@@ -10,37 +10,25 @@ namespace PkcsExtenions.Tests
 {
     internal static class CertificateGenerator
     {
-        public static X509Certificate2 Create(string cn)
+        public static X509Certificate2 Create(string cn, X509KeyUsageFlags? usageFlags = null, string extraKeyUsageOids = null)
         {
             using RSA rsaKeys = RSA.Create(2048);
 
             CertificateRequest request = new CertificateRequest($"CN={cn}; C=SK", rsaKeys, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
-            return request.CreateSelfSigned(DateTime.Now.AddDays(-1.0), DateTime.Now.AddDays(1.0));
-        }
-
-        public static X509Certificate2 Create(string cn, X509KeyUsageFlags usageFlags)
-        {
-            using RSA rsaKeys = RSA.Create(2048);
-
-            CertificateRequest request = new CertificateRequest($"CN={cn}; C=SK", rsaKeys, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
-            request.CertificateExtensions.Add(new X509KeyUsageExtension(usageFlags, false));
-
-            return request.CreateSelfSigned(DateTime.Now.AddDays(-1.0), DateTime.Now.AddDays(1.0));
-        }
-
-        public static X509Certificate2 Create(string cn, X509KeyUsageFlags usageFlags, string extraKeyUsageOids)
-        {
-            using RSA rsaKeys = RSA.Create(2048);
-
-            CertificateRequest request = new CertificateRequest($"CN={cn}; C=SK", rsaKeys, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
-            request.CertificateExtensions.Add(new X509KeyUsageExtension(usageFlags, false));
-
-            OidCollection oidCollection = new OidCollection();
-            foreach(string oid in extraKeyUsageOids.Split(new char[] { ';', ',' }))
+            if (usageFlags.HasValue)
             {
-                oidCollection.Add(new Oid(oid.Trim()));
+                request.CertificateExtensions.Add(new X509KeyUsageExtension(usageFlags.Value, false));
             }
-            request.CertificateExtensions.Add(new X509EnhancedKeyUsageExtension(oidCollection, false));
+
+            if (!string.IsNullOrEmpty(extraKeyUsageOids))
+            {
+                OidCollection oidCollection = new OidCollection();
+                foreach (string oid in extraKeyUsageOids.Split(new char[] { ';', ',' }))
+                {
+                    oidCollection.Add(new Oid(oid.Trim()));
+                }
+                request.CertificateExtensions.Add(new X509EnhancedKeyUsageExtension(oidCollection, false));
+            }
 
             return request.CreateSelfSigned(DateTime.Now.AddDays(-1.0), DateTime.Now.AddDays(1.0));
         }
