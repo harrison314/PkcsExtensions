5933df5db10af05b4b43cb688e701974601ca063
harrison314 <harrison314@azet.sk>
2020-05-04 18:04:18 +0200

    Implementation of HashAlgorithmWrapper.

---
+++ src/src/PkcsExtenions/Algorithms/HashAlgorithmWrapper.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PkcsExtenions.Algorithms
+{
+    public class HashAlgorithmWrapper
+    {
+        private readonly HashAlgorithm hashAlgorithm;
+
+        public int HashSizeInBytes
+        {
+            get => this.hashAlgorithm.HashSize / 8;
+        }
+
+        public HashAlgorithmWrapper(HashAlgorithm hashAlgorithm)
+        {
+            this.hashAlgorithm = hashAlgorithm ?? throw new ArgumentNullException(nameof(hashAlgorithm));
+        }
+
+        public void Clear()
+        {
+            this.hashAlgorithm.Clear();
+        }
+
+        public void Update(byte[] input)
+        {
+            ThrowHelpers.CheckNull(nameof(input), input);
+
+            int offset = 0;
+            int size = this.hashAlgorithm.HashSize / 8;
+
+            while (input.Length - offset >= size)
+            {
+                offset += this.hashAlgorithm.TransformBlock(input, offset, size, null, 0);
+            }
+
+            if (input.Length - offset > 0)
+            {
+                this.hashAlgorithm.TransformBlock(input, offset, input.Length - offset, null, 0);
+            }
+        }
+
+        public void Update(byte[] input, int start, int length)
+        {
+            ThrowHelpers.CheckNull(nameof(input), input);
+            if (start < 0 || start >= input.Length) throw new ArgumentOutOfRangeException(nameof(start));
+            if (length < 0 || start + length > input.Length) throw new ArgumentOutOfRangeException(nameof(length));
+
+            int offset = start;
+            int size = this.hashAlgorithm.HashSize / 8;
+
+            while (start + length - offset >= size)
+            {
+                offset += this.hashAlgorithm.TransformBlock(input, offset, size, null, 0);
+            }
+
+            if (start + length - offset > 0)
+            {
+                this.hashAlgorithm.TransformBlock(input, offset, input.Length - offset, null, 0);
+            }
+        }
+
+        public byte[] DoFinal()
+        {
+            byte[] result = new byte[this.hashAlgorithm.HashSize / 8];
+            this.hashAlgorithm.TransformFinalBlock(Array.Empty<byte>(), 0, 0);
+            Buffer.BlockCopy(this.hashAlgorithm.Hash, 0, result, 0, result.Length);
+
+            return result;
+        }
+
+        public bool TryDoFinal(Span<byte> hash, out int writeBytes)
+        {
+            int size = this.hashAlgorithm.HashSize / 8;
+            if (hash.Length < size)
+            {
+                writeBytes = 0;
+                return false;
+            }
+
+            this.hashAlgorithm.TransformFinalBlock(Array.Empty<byte>(), 0, 0);
+            (new Span<byte>(this.hashAlgorithm.Hash)).CopyTo(hash);
+
+            writeBytes = size;
+            return true;
+        }
+    }
+}
---
+++ src/test/PkcsExtenions.Tests/Algorithms/HashAlgorithmWrapperTests.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PkcsExtenions.Algorithms;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PkcsExtenions.Tests.Algorithms
+{
+    [TestClass]
+    public class HashAlgorithmWrapperTests
+    {
+        [TestMethod]
+        public void Clear()
+        {
+            using SHA1 sha1 = SHA1.Create();
+            HashAlgorithmWrapper hashAlgorithmWrapper = new HashAlgorithmWrapper(sha1);
+
+            hashAlgorithmWrapper.Clear();
+        }
+
+        [TestMethod]
+        public void UpdateWithArray()
+        {
+            using SHA1 sha1 = SHA1.Create();
+            HashAlgorithmWrapper hashAlgorithmWrapper = new HashAlgorithmWrapper(sha1);
+
+            byte[] data1 = new byte[2];
+            byte[] data2 = new byte[20];
+            byte[] data3 = new byte[147];
+
+            Random r = new Random(42);
+            r.NextBytes(data1);
+            r.NextBytes(data2);
+            r.NextBytes(data3);
+
+            using MemoryStream ms = new MemoryStream();
+            ms.Write(data1);
+            ms.Write(data2);
+            ms.Write(data3);
+            ms.Position = 0L;
+            using SHA1 alternative = SHA1.Create();
+
+            byte[] exceptedHash = alternative.ComputeHash(ms);
+
+            hashAlgorithmWrapper.Update(data1);
+            hashAlgorithmWrapper.Update(data2);
+            hashAlgorithmWrapper.Update(data3);
+
+            byte[] hash = hashAlgorithmWrapper.DoFinal();
+
+            CollectionAssert.AreEquivalent(exceptedHash, hash, "Error in cputed hash.");
+        }
+
+        [TestMethod]
+        public void UpdateWithSubArray()
+        {
+            using SHA1 sha1 = SHA1.Create();
+            HashAlgorithmWrapper hashAlgorithmWrapper = new HashAlgorithmWrapper(sha1);
+
+            byte[] data1 = new byte[2];
+            byte[] data2 = new byte[24];
+            byte[] data3 = new byte[147];
+
+            Random r = new Random(42);
+            r.NextBytes(data1);
+            r.NextBytes(data2);
+            r.NextBytes(data3);
+
+            using MemoryStream ms = new MemoryStream();
+            ms.Write(data1, 0, 2);
+            ms.Write(data2, 4, 20);
+            ms.Write(data3, 13, 120);
+            ms.Position = 0L;
+            using SHA1 alternative = SHA1.Create();
+
+            byte[] exceptedHash = alternative.ComputeHash(ms);
+
+            hashAlgorithmWrapper.Update(data1, 0, 2);
+            hashAlgorithmWrapper.Update(data2, 4, 20);
+            hashAlgorithmWrapper.Update(data3, 13, 120);
+
+            byte[] hash = hashAlgorithmWrapper.DoFinal();
+
+            CollectionAssert.AreEquivalent(exceptedHash, hash, "Error in cputed hash.");
+        }
+
+        [TestMethod]
+        public void DoFinal()
+        {
+            using SHA1 sha1 = SHA1.Create();
+            HashAlgorithmWrapper hashAlgorithmWrapper = new HashAlgorithmWrapper(sha1);
+
+            byte[] data3 = new byte[147];
+
+            Random r = new Random(42);
+            r.NextBytes(data3);
+    
+            using SHA1 alternative = SHA1.Create();
+
+            byte[] exceptedHash = alternative.ComputeHash(data3);
+
+            hashAlgorithmWrapper.Update(data3);
+
+            byte[] hash = hashAlgorithmWrapper.DoFinal();
+
+            CollectionAssert.AreEquivalent(exceptedHash, hash, "Error in cputed hash.");
+        }
+
+        [TestMethod]
+        public void TryDoFinal()
+        {
+            using SHA1 sha1 = SHA1.Create();
+            HashAlgorithmWrapper hashAlgorithmWrapper = new HashAlgorithmWrapper(sha1);
+
+            byte[] data3 = new byte[147];
+
+            Random r = new Random(42);
+            r.NextBytes(data3);
+
+            using SHA1 alternative = SHA1.Create();
+
+            byte[] exceptedHash = alternative.ComputeHash(data3);
+
+            hashAlgorithmWrapper.Update(data3);
+
+            byte[] hash = new byte[80];
+            Assert.IsFalse(hashAlgorithmWrapper.TryDoFinal(new Span<byte>(hash, 0, 5), out _));
+            Assert.IsTrue(hashAlgorithmWrapper.TryDoFinal(new Span<byte>(hash, 0, 78), out int witeBytes));
+
+            byte[] subArray = hash.Take(witeBytes).ToArray();
+            CollectionAssert.AreEquivalent(exceptedHash, subArray, "Error in cputed hash.");
+            Assert.AreEqual(20, witeBytes);
+        }
+    }
+}
