8b21d83b69abbac41f2cac493df27a03c5423e3c
harrison314 <harrison314@azet.sk>
2020-05-09 17:54:38 +0200

    Add PEM formater.

---
+++ src/src/PkcsExtenions/PemFormater.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PkcsExtenions
+{
+    public static class PemFormater
+    {
+        public static string ToPem(ReadOnlySpan<byte> data, string name)
+        {
+            ThrowHelpers.CheckNullOrEempty(nameof(name), name);
+
+            return string.Concat("-----BEGIN ", name, "-----\r\n",
+                Convert.ToBase64String(data, Base64FormattingOptions.InsertLineBreaks),
+                "\r\n-----END ", name, " -----");
+        }
+
+        public static ReadOnlySpan<byte> FromDerOrPem(ReadOnlySpan<byte> data)
+        {
+            if (data.Length < 16) return data;
+
+            if (data[0] == 45 && data[1] == 45 && data[2] == 45)
+            {
+                string pem = Encoding.ASCII.GetString(data);
+                Match match = Regex.Match(pem, "-----BEGIN .+?-----(.+?)-----END .+?-----", RegexOptions.Singleline);
+                if (!match.Success)
+                {
+                    throw new ArgumentException("data is not valid PEM");
+                }
+
+                return Convert.FromBase64String(match.Groups[1].Value);
+            }
+            else
+            {
+                return data;
+            }
+        }
+    }
+}
---
+++ src/test/PkcsExtenions.Tests/PemFormaterTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PkcsExtenions.Tests
+{
+    [TestClass]
+    public class PemFormaterTests
+    {
+        [TestMethod]
+        public void ToPem()
+        {
+            string result = PemFormater.ToPem(new byte[200], "NULLS");
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void FromDerOrPem()
+        {
+            string certificatePem = @"-----BEGIN CERTIFICATE-----
+MIIG4jCCBMqgAwIBAgIJAJjguYVnU08GMA0GCSqGSIb3DQEBBQUAMIGmMQswCQYD
+VQQGEwJVUzEOMAwGA1UECBMFVGV4YXMxFDASBgNVBAcTC1NhbiBBbnRvbmlvMRow
+GAYDVQQKExFHbG9iYWxTQ0FQRSwgSW5jLjEUMBIGA1UECxMLRW5naW5lZXJpbmcx
+FTATBgNVBAMTDG1pa2Utcm9vdC1jYTEoMCYGCSqGSIb3DQEJARYZbWhhbWJpZGdl
+QGdsb2JhbHNjYXBlLmNvbTAeFw0xMDExMTgyMTE5NDdaFw0xNTExMTcyMTE5NDda
+MIGmMQswCQYDVQQGEwJVUzEOMAwGA1UECBMFVGV4YXMxFDASBgNVBAcTC1NhbiBB
+bnRvbmlvMRowGAYDVQQKExFHbG9iYWxTQ0FQRSwgSW5jLjEUMBIGA1UECxMLRW5n
+aW5lZXJpbmcxFTATBgNVBAMTDG1pa2Utcm9vdC1jYTEoMCYGCSqGSIb3DQEJARYZ
+bWhhbWJpZGdlQGdsb2JhbHNjYXBlLmNvbTCCAiIwDQYJKoZIhvcNAQEBBQADggIP
+xYK3mO1034kBdDxmVoBeEwfjWWPyC/uyFGwCNZCzoAQGMxNAnj33NBiCLHJRo1Z5
+BxirSSMxOT4LEkmkOhuTyKB0TJZf+8wP8pK5BsO3xjO+uM1K3LY=
+-----END CERTIFICATE-----";
+
+            byte[] pemData = Encoding.UTF8.GetBytes(certificatePem);
+
+            ReadOnlySpan<byte> der = PemFormater.FromDerOrPem(pemData);
+            Assert.AreNotEqual(0, der.Length);
+        }
+    }
+}
