1dada7ff2673eca686203a5d06c59c0373bc01c7
harrison314 <harrison314@azet.sk>
2020-05-22 20:14:13 +0200

    #4 Add example for Pkcs1DigestInfo.

---
+++ src/test/PkcsExtensions.UsageTests/Pkcs1/Pkcs1DigestInfoTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PkcsExtensions.Pkcs1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PkcsExtensions.UsageTests.Pkcs1
+{
+    [TestClass]
+    public class Pkcs1DigestInfoTests
+    {
+        [TestMethod]
+        public void Example_Pkcs1DigestInfo()
+        {
+            byte[] dataToSign = Encoding.ASCII.GetBytes("Hello world");
+            using SHA256 sha = SHA256.Create();
+            byte[] hashToSign = sha.ComputeHash(dataToSign);
+
+           byte[] digestInfo = Pkcs1DigestInfo.Encode(HashAlgorithmName.SHA256, hashToSign);
+
+            SignUsingPkcs11(digestInfo);
+        }
+
+        private static void SignUsingPkcs11(byte[] digestInfo)
+        {
+            // Only for example
+        }
+    }
+}
