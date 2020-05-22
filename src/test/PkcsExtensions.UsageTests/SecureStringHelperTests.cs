ea4e754e61a9943b83000a4b6ecc57a44541c4e6
harrison314 <harrison314@azet.sk>
2020-05-22 20:23:11 +0200

    Add example for PkcsExtensions #4

---
+++ src/test/PkcsExtensions.UsageTests/SecureStringHelperTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PkcsExtensions.UsageTests
+{
+    [TestClass]
+    public class SecureStringHelperTests
+    {
+        [TestMethod]
+        public void Example_SecureStringHelper()
+        {
+            SecureString pin = this.ObtrainPin();
+
+            SecureStringHelper.ExecuteWithSecureString(pin, Encoding.UTF8, pin =>
+            {
+                SetPinToDevice(pin);
+            });
+        }
+
+        private SecureString ObtrainPin()
+        {
+            // Only for example
+
+            SecureString pin = new SecureString();
+            pin.AppendChar('4');
+            pin.AppendChar('5');
+            pin.AppendChar('6');
+            pin.AppendChar('1');
+            pin.AppendChar('2');
+            pin.AppendChar('3');
+            pin.AppendChar('7');
+            pin.AppendChar('8');
+            pin.AppendChar('9');
+
+            return pin;
+        }
+
+        private static void SetPinToDevice(byte[] pin)
+        {
+            // Only for example
+        }
+    }
+}
